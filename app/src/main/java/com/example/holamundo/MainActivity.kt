22f68efdf9d1f9ecbb7251d8e1c73498a9cc1a91
LuisRocha <149401326+Luiss2080@users.noreply.github.com>
2026-01-05 09:35:24 -0400

    Primer Formulario, correo y nombre

--- app/src/main/java/com/example/holamundo/MainActivity.kt
+++ app/src/main/java/com/example/holamundo/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.holamundo.ui.theme.HolaMundoTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             HolaMundoTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Mundoo",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    RegistrationForm(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +38,35 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun RegistrationForm(modifier: Modifier = Modifier) {
+    var name by remember { mutableStateOf("") }
+    var email by remember { mutableStateOf("") }
+
+    Column(
+        modifier = modifier.padding(16.dp),
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        TextField(
+            value = name,
+            onValueChange = { name = it },
+            label = { Text("Name") }
+        )
+        TextField(
+            value = email,
+            onValueChange = { email = it },
+            label = { Text("Email") }
+        )
+        Button(onClick = { /* Handle form submission */ }) {
+            Text("Submit")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun RegistrationFormPreview() {
     HolaMundoTheme {
-        Greeting("Android")
+        RegistrationForm()
     }
 }
